39bce17013839a4c64e71724eef005efa851161c
massun-onibakuchi <etherdao0x00@gmail.com>
2021-10-23 18:37:39 +0900

    fix:strategyToken : stETH=> WstETH, underlying : wETH =>  stETH

--- contracts/IdleLidoStrategy.sol
+++ contracts/IdleLidoStrategy.sol
@@ -3,11 +3,9 @@ pragma solidity 0.8.7;
 
 import "./interfaces/IIdleCDOStrategy.sol";
 import "./interfaces/IERC20Detailed.sol";
-import "./interfaces/IWETH.sol";
-import "./interfaces/IStableSwapSTETH.sol";
-import "./interfaces/IStETHPriceFeed.sol";
-import "./interfaces/ILidoOracle.sol";
 import "./interfaces/ILido.sol";
+import "./interfaces/ILidoOracle.sol";
+import "./interfaces/IWstETH.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
@@ -33,27 +31,17 @@ contract IdleLidoStrategy is
     uint256 public constant ONE_STETH_TOKEN = 10**18;
     /// @notice seconds in year
     uint256 private constant SECONDS_IN_YEAR = 365 * 24 * 60 * 60;
-    /// @notice address of the strategy used, in this case Lido StETH
+    /// @notice address of the strategy used, in this case wstETH
     address public override strategyToken;
-    /// @notice underlying token address (WETH)
+    /// @notice underlying token address (stETH)
     address public override token;
     /// @notice one underlying token
     uint256 public override oneToken;
     /// @notice decimals of the underlying asset
     uint256 public override tokenDecimals;
-    /// @notice underlying ERC20 token contract
-    IERC20Detailed public underlyingToken;
-    /// @notice lido contract
-    ILido public lido;
-    /// @notice Curve Fi StableSwap STETH/ETH
-    IStableSwapSTETH public stableSwap;
-    /// @notice Lido stETH/ETH price feed
-    IStETHPriceFeed public priceFeed;
 
-    /// @notice referral Lido
-    address public referral;
-    /// @notice stETH=>ETH
-    uint256 public slipageBps;
+    /// @notice lido stETH contract
+    ILido public lido;
 
     address public whitelistedCDO;
 
@@ -72,16 +60,12 @@ contract IdleLidoStrategy is
     /// @notice can only be called once
     /// @dev Initialize the upgradable contract
     /// @param _strategyToken address of the strategy token
-    /// @param _wethToken address of WETH
+    /// @param _underlyingToken address of stETH
     /// @param _owner owner address
     function initialize(
         address _strategyToken,
-        address _wethToken,
-        address _priceFeed,
-        address _stableSwap,
-        address _owner,
-        address _referral,
-        uint256 _slipageBps
+        address _underlyingToken,
+        address _owner
     ) public initializer {
         require(token == address(0), "Initialized");
         // Initialize contracts
@@ -89,17 +73,12 @@ contract IdleLidoStrategy is
         ReentrancyGuardUpgradeable.__ReentrancyGuard_init();
         // Set basic parameters
         strategyToken = _strategyToken;
-        token = _wethToken;
-        lido = ILido(_strategyToken);
-        priceFeed = IStETHPriceFeed(_priceFeed);
-        stableSwap = IStableSwapSTETH(_stableSwap);
-        referral = _referral;
-        slipageBps = _slipageBps;
-
+        token = _underlyingToken;
+        lido = ILido(_underlyingToken);
         tokenDecimals = IERC20Detailed(token).decimals();
         oneToken = 10**(tokenDecimals);
-        underlyingToken = IERC20Detailed(token);
 
+        lido.safeApprove(_strategyToken,type(uint256).max);
         // transfer ownership
         transferOwnership(_owner);
     }
@@ -117,18 +96,15 @@ contract IdleLidoStrategy is
         returns (uint256 minted)
     {
         if (_amount > 0) {
-            ILido _lido = lido;
             /// get `tokens` from msg.sender
-            underlyingToken.safeTransferFrom(
+            lido.safeTransferFrom(
                 msg.sender,
                 address(this),
                 _amount
             );
-            IWETH(token).withdraw(_amount);
-            /// deposit those in lido
-            minted = _lido.submit{value: _amount}(referral);
+            minted = IWstETH(strategyToken).wrap(_amount);
             /// transfer stETH to msg.sender
-            _lido.safeTransfer(msg.sender, minted);
+            IERC20Detailed(strategyToken).safeTransfer(msg.sender, minted);
         }
     }
 
@@ -180,34 +156,15 @@ contract IdleLidoStrategy is
     /// @return redeemed amount of underlyings redeemed
     function _redeem(uint256 _amount) internal returns (uint256 redeemed) {
         if (_amount > 0) {
-            ILido _lido = lido;
             // get stETH from the user
-            _lido.safeTransferFrom(msg.sender, address(this), _amount);
-            // swap stETH for ETH
-            _lido.safeApprove(address(stableSwap), _amount);
-            _swapStETHForETH(_amount, slipageBps);
-            // wrap ETH
-            redeemed = address(this).balance;
-            IWETH(token).deposit{value: redeemed}();
-            // transfer WETH to msg.sender
-            underlyingToken.safeTransfer(msg.sender, redeemed);
+            IERC20Detailed(strategyToken).safeTransferFrom(msg.sender, address(this), _amount);
+            // redeem underlyings from Idle
+            redeemed = IWstETH(strategyToken).unwrap(_amount);
+            // transfer underlyings to msg.sender
+            lido.safeTransfer(msg.sender, redeemed);
             // transfer gov tokens to msg.sender
             _withdrawGovToken(msg.sender);
         }
-    }
-
-    /// @dev Swap stETH for ETH on the specified Curve stETH/ETH pool
-    /// @param _amountStETH input amount of stETH 
-    /// @param _slipageBps slipage tollerance
-    /// @return amountOutETH 
-    function _swapStETHForETH(uint256 _amountStETH, uint256 _slipageBps)
-        internal
-        returns (uint256 amountOutETH)
-    {
-        //1 bp = 0.01 %
-        uint256 minETH = _amountStETH - (_amountStETH * _slipageBps) / 10000;
-        // swap stETH for ETH using Curve stETH/ETH stableSwap.
-        amountOutETH = stableSwap.exchange(1, 0, _amountStETH, minETH);
     }
 
     // ###################
@@ -216,8 +173,7 @@ contract IdleLidoStrategy is
 
     /// @return net price in underlyings of 1 strategyToken
     function price() public view override returns (uint256) {
-        (uint256 price_, ) = priceFeed.safe_price();
-        return price_;
+        return IWstETH(strategyToken).stEthPerToken();
     }
 
     /// @dev Lido stETH: calculation of staker rewards  https://docs.lido.fi/contracts/lido-oracle#add-calculation-of-staker-rewards-apr
@@ -277,12 +233,5 @@ contract IdleLidoStrategy is
     function setWhitelistedCDO(address _cdo) external onlyOwner {
         require(_cdo != address(0), "IS_0");
         whitelistedCDO = _cdo;
-    }
-
-    receive() external payable {
-        require(
-            msg.sender == token || msg.sender == address(stableSwap),
-            "only-weth-or-stableswap"
-        );
     }
 }
